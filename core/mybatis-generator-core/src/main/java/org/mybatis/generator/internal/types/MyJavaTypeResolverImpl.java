76bfaa73550fd0c4a1d305938b4c6f344ad9b52a
张松 <song.z@shulidata.com>
2022-01-10 20:09:00 +0800

    feat:调整注释等

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/internal/types/MyJavaTypeResolverImpl.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/internal/types/MyJavaTypeResolverImpl.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2006-2021 the original author or authors.
+ *    Copyright 2006-2022 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ import java.sql.Types;
 
 /**
  * @author song.z
- * @date 2021/12/31 10:52 下午
  */
 public class MyJavaTypeResolverImpl extends JavaTypeResolverDefaultImpl {
     public MyJavaTypeResolverImpl() {
